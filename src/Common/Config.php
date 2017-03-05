e8964c9223d8470375147551085d2a0ccde0ea3d
robmachado <linux.rlm@gmail.com>
2017-03-05 17:53:12 -0300

    22 comm

---
+++ src/Common/Config.php
@@ -0,0 +1,62 @@
+<?php
+namespace Aenet\NFe\Common;
+
+class Config
+{
+    public $tpAmb = 2;
+    public $razaosocial;
+    public $siglaUF;
+    public $cnpj;
+    public $schemes;
+    public $versao;
+    public $tokenIBPT;
+    public $CSC;
+    public $CSCid;
+    public $aProxyConf = [
+        'proxyIp' => '',
+        'proxyPort' => '',
+        'proxyUser' => '',
+        'proxyPass' => ''
+    ];        
+    
+    public function __construct(
+        $razaosocial,
+        $siglaUF,
+        $cnpj,
+        $tpAmb = 2,
+        $schemes = 'PL_008i2',
+        $versao = '3.10',
+        $tokenIBPT = '',
+        $CSC = '',
+        $CSCid = '',
+        $aProxyConf = []
+    ) {
+        $this->tpAmb = $tpAmb;
+        $this->razaosocial = $razaosocial;
+        $this->siglaUF = $siglaUF;
+        $this->cnpj = $cnpj;
+        $this->schemes = $schemes;
+        $this->versao = $versao;
+        $this->tokenIBPT = $tokenIBPT;
+        $this->CSC = $CSC;
+        $this->CSCid = $CSCid;
+        $this->aProxyConf = $aProxyConf;
+    }
+    
+    public function __toString() {
+        $conf = [
+            'atualizacao' => date('Y-=m-d H:i:s'),
+            'tpAmb' => $this->tpAmb,
+            'razaosocial' => $this->razaosocial,
+            'siglaUF' => $this->siglaUF,
+            'cnpj' => $this->cnpj,
+            'schemes' => $this->schemes,
+            'versao' => $this->versao,
+            'tokenIBPT' => $this->tokenIBPT,
+            'CSC' => $this->CSC,
+            'CSCid' => $this->CSCid,
+            'aProxyConf' => $this->aProxyConf
+        ];
+        return json_encode($conf);
+    }
+}
--- src/Controllers/StatusController.php
+++ src/Controllers/StatusController.php
@@ -8,35 +8,67 @@ use NFePHP\Common\Soap\SoapCurl;
 use NFePHP\Common\Certificate\CertificationChain;
 use stdClass;
 use Aenet\NFe\Models\Status;
+use Aenet\NFe\Common\Config;
 
 class StatusController
 {
-    protected $cadastro;
+    public $uf;
+    public $tpAmb;
+    public $status;
+    public $error = '';
     
+    protected $cad;
+    protected $config;
+
     public function __construct(stdClass $cad)
     {
-        $this->cadastro = $cad;
+        $this->cad = $cad;
+        $config = new Config(
+            'Qualquer coisa',
+            $cad->uf,    
+            $cad->cnpj,
+            $cad->tpAmb
+        );
+        $this->config = "{$config}";
     }
     
     /**
      * Pull status from SEFAZ
      * @param string $uf
+     * @param int $tpAmb
      */
-    public function pull($uf, $tpAmb)
+    public function pull($uf = null, $tpAmb = null)
     {
-        
-        
-        //$chain = new CertificationChain(file_get_contents('chain.pem'));
-        $certificate = Certificate::readPfx($content, 'fima');
-        //$certificate->chainKeys = $chain;
-        $tools = new Tools($configJson, $certificate);
-        $tools->model('55');
-        $soap = new SoapCurl();
-        $soap->setDebugMode(false);
-        $tools->loadSoapClass($soap);
-        $tools->tpAmb = $tpAmb;
-        $response = $tools->sefazStatus($uf);
-        
+        try {
+            //carrega o certificado
+            $certificate = Certificate::readPfx(
+                base64_decode($this->cad->crtpfx),
+                $this->cad->crtpass
+            );
+            $certificate->chainKeys = new CertificationChain(
+                $this->cad->crtchain
+            );
+            //carrega a classe de comunicação
+            $tools = new Tools($configJson, $certificate);
+            $tools->model('55');
+            $soap = new SoapCurl();
+            $soap->setDebugMode(false);
+            $tools->loadSoapClass($soap);
+            if (!empty($tpAmb)) {
+                //corrige o ambiente
+                $tools->tpAmb = $tpAmb;
+            }    
+            if (empty($uf)) {
+                $uf = $this->cad->uf;
+            }
+            //executa a chama SOAP
+            $response = $tools->sefazStatus($uf);
+            return $reposnse;
+        } catch (InvalidArgumentException $e) {
+            
+        } catch (Exception $e) {
+            
+        }
     }
     
     /**
