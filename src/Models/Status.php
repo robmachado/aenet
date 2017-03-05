ea4aa12142aebbd64beb7c3e58023fa8b951996f
robmachado <linux.rlm@gmail.com>
2017-03-05 12:17:59 -0300

    18 comm

---
+++ src/Models/Status.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aenet\NFe\Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Status extends Eloquent
+{
+    public $timestamps = false;
+    protected $table = 'sefaz_status';
+    protected $fillable = [
+        'uf',
+        'status_1',
+        'error_msg_1',
+        'updated_at_1',
+        'status_2',
+        'error_msg_2',
+        'updated_at_2'
+
+    ];
+}
