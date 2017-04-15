a5e5477186a78538c152b6c305e92afe0d3a40eb
robmachado <linux.rlm@gmail.com>
2017-04-15 11:57:36 -0300

    [UPD] ajustes

--- jobs/job_clearlogs.sh
+++ jobs/job_clearlogs.sh
@@ -3,3 +3,5 @@
 echo "" > /var/www/aenet/storage/job_status.log
 echo "" > /var/www/aenet/storage/job_dfe.log
 echo "" > /var/www/aenet/storage/job_nfe.log
+echo "" > /var/www/aenet/storage/job_danfe.log
+echo "" > /var/www/aenet/storage/job_mail.log
